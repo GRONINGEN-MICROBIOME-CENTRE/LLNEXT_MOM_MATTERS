e6875c8b17af4fcbc588aa3eeba00f4c49874b61
Trishla Sinha <40964388+trishlasinha@users.noreply.github.com>
2026-07-30 09:55:06 +0200

    Clean up commented-out mother data processing code
    Removed commented-out code related to mother metadata processing and PERMANOVA analysis.

--- Delivery mode shapes the infant gut microbiome/TCAM/LLNEXT_Mom_Matters_TCAM_PERMANOVA.R
+++ Delivery mode shapes the infant gut microbiome/TCAM/LLNEXT_Mom_Matters_TCAM_PERMANOVA.R
@@ -1,5 +1,6 @@
 ################### PERMANOVA TCAM INFANTS ##########################
 # Author: T.Sinha 
+# Permanova analysis of TCAM factors with predictors in infants 
 # Last updated: 28th September, 2025
 
 library(tidyverse)
@@ -150,61 +151,6 @@ write.table(ResultsAdonis_cor_delivery_feeding, "result_adonis_TCAM_INFANTS_BY_M
 
 
 
-
-##############################
-# Mother 
-##############################
-setwd("/Users/trishlasinha/Desktop/LLNEXT/Analysis/phenotypes")
-metadata_mothers<-read.delim("mother_phenotypes_all_microbiome_fil.txt")
-metadata_mothers <- subset(metadata_mothers, Timepoint_categorical != "M1" & Timepoint_categorical != "M2")
-row.names(metadata_mothers)<-metadata_mothers$NG_ID
-
-taxa<-read.delim("~/Desktop/LLNEXT/Analysis/taxa/LLNEXT_metaphlan_4_CLEAN_10_07_2023.txt")
-mother_taxa<-taxa[row.names(taxa)%in% rownames(metadata_mothers),] 
-mother_taxa<-mother_taxa[match(row.names(metadata_mothers),row.names(mother_taxa)),]
-
-# Filtering data 
-mother_NEXT_ID<-metadata_mothers %>%
-  select(next_id_mother)
-mother_taxa_all<-merge(mother_NEXT_ID,mother_taxa, by="row.names" )
-row.names(mother_taxa_all)<-mother_taxa_all$Row.names
-mother_taxa_all$Row.names=NULL
-unique_counts <- sapply(mother_taxa_all, function(x) length(unique(mother_taxa_all$next_id_mother[x >0.001]))) # Here I am counting the unique elements in the NEXT_ID column where the corresponding value in each column (i.e., x) is greater than the given cut-off. 
-mother_taxa_all_filt <- mother_taxa_all[, unique_counts >= 0.3*length(unique(mother_taxa_all$next_id_mother)) ] 
-mother_taxa_all_filt$next_id_mother=NULL
-
-mother_taxa_SGB<-mother_taxa_all_filt[,grep("t__",colnames(mother_taxa_all_filt))]
-my_pseudocount_normal=min(mother_taxa_SGB[mother_taxa_SGB!=0])/2# 
-distance=vegdist(mother_taxa_SGB, method = "aitchison", pseudocount=my_pseudocount_normal) 
-
-
-
-
-not_to_test<- c("NG_ID", "NEXT_ID", "FAMILY", "raw_reads_FQ_1", "raw_reads_FQ_2","next_id_mother","Modified_NEXT_ID_without_preg_number" ,"days_from_first_collection"  ,
-                "human_reads_FQ_1", "human_reads_FQ_2", "clean_reads_FQ_1", "clean_reads_FQ_2",
-                "reads_lost_QC", "dna_conc", "isolation_method", "NG_ID_short",
-                "exact_age_days_at_collection", "exact_age_months_at_collection",
-                "Timepoint_categorical", "SAMPLE_ID",
-                "metaphlan4_unclassified", "contaminant_1_Sphingomonas_sp_FARSPH",
-                "contaminant_2_Phyllobacterium_myrsinacearum", "metaphlan4_unclassified_with_contaminants",
-                "shannon", "BATCH_NUMBER", "next_id_partner", "sibling_number", "timepoint")
-
-
-
-for (Timepoint in unique(metadata_mothers$Timepoint_categorical)){
-  print(Timepoint)
-  phenotypes<-metadata_mothers %>% filter(Timepoint_categorical == Timepoint )
-  PP=drop_phenotypes(phenotypes, non_NA_values = 100, minimum_variance = 5)
-  PP %>% select(-one_of(not_to_test)) %>% colnames() -> PhenosTest
-  #input selected phenotypes, as a vector, to Phenotype_list
-  Call_adonis(phenotypes, Phenotype_list=PhenosTest ,perm=10000, Distance=distance) %>% mutate(Timepoint = Timepoint) %>% rbind(ResultsAdonis, .) -> ResultsAdonis
-}
-
-
-ResultsAdonis$Timepoint <- factor(ResultsAdonis$Timepoint, levels = c("P12", "P28", "B", "M3"))
-
-setwd("/Users/trishlasinha/Desktop/LLNEXT/Analysis/results/adonis_per_timepoint")
-write.table(ResultsAdonis, "result_adonis_per_timepoint_mothers_10000_perm.txt", sep = "\t", row.names = F)
 
 
 
